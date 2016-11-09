378ae186a4d5f5a944b5e1979b855af20f218d39
Connor Sewell <ctrsewell@dundee.ac.uk>
2016-11-09 17:25:06 +0000

    Adding more methods for comparison. Began looking at entropy.

--- app/utils/HistogramHandler.py
+++ app/utils/HistogramHandler.py
@@ -6,6 +6,17 @@ import feature_extractor
 import numpy as np
 
 
+#http://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/histogram_comparison/histogram_comparison.html
+
+def calc_Correlation(histogram1, histogram2):
+    correlation = cv2.compareHist(histogram1, histogram2, method=cv2.HISTCMP_CORREL)
+
+
+def calc_bhattacharyya_dist(histogram1, histogram2):
+    distance = cv2.compareHist(histogram1, histogram2, method=cv2.HISTCMP_BHATTACHARYYA)
+    return distance
+
+
 def calc_chi_squared_dist(histogram1, histogram2):
     distance = cv2.compareHist(histogram1, histogram2, method=cv2.HISTCMP_CHISQR)
     return distance
@@ -28,8 +39,32 @@ def calc_histogram(image):
     hist = cv2.calcHist(image_hsv, [0, 1], None, [180, 256], [0, 180, 0, 256])
     #http://stackoverflow.com/questions/9390592/drawing-histogram-in-opencv-python
     cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX)
-    return hist;
+    return hist
+
+
+#http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_gradients/py_gradients.html
+#def sobel_comparison(image1, image2):
+#    image1_x_sobel = cv2.Sobel(image1, cv2.CV_64F, 1, 0, ksize=5)
+#    image1_y_sobel = cv2.Sobel(image1, cv2.CV_64F, 0, 1, ksize=5)
+#    image2_x_sobel = cv2.Sobel(image2, cv2.CV_64F, 1, 0, ksize=5)
+#    image2_y_sobel = cv2.Sobel(image2, cv2.CV_64F, 0, 1, ksize=5)
+#    return 0
+
+
+#def compare_entropy(histogram1, histogram2):
+#    return 0
+
+
+#def calculate_entropy(histogram, bin_count):
+#    for i in range(0, bin_count):
+#        print histogram[i]
+
+#    positive_entropy = histogram
+#    entropy = -positive_entropy
+#    return entropy
 
 
 #https://www.mathworks.com/help/stats/knnsearch.html
 #^ For Part B
+
+#http://stackoverflow.com/questions/189943/how-can-i-quantify-difference-between-two-images
